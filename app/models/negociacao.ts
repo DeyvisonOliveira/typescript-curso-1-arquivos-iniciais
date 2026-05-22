1d7689b463e6a1dc7c3704a87d7df028aa05703c
DeyvisonOliveira <deyvison_1999@outlook.com.br>
2026-05-21 22:50:40 -0300

    CursoPartUM

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,26 +1,11 @@
 export class Negociacao {
-     private _data;
-     private _quantidade;
-     private _valor;
 
-     constructor(data, quantidade, valor) {
-          this._data = data;
-          this._quantidade = quantidade;
-          this._valor = valor;
-     } 
-     get data() {
-          return this._data;
+     constructor(
+          public  readonly data: Date, 
+          public readonly quantidade: number, 
+          public readonly valor: number) {}
+
+     get volume(): number {
+          return this.quantidade * this.valor;
      }
-     get quantidade() {
-          return this._quantidade;
-     }    
-     get valor() {
-          return this._valor;
-     }
-
-     get volume() {
-          return this._quantidade * this._valor;
-     }
-
-
 }
